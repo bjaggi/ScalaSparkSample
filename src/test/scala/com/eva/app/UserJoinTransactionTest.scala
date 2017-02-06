eee262c0cf653a6e3fdc86301e55055c776442a4
Brijesh Jaggi <brijesh.jaggi@gmail.com>
2017-02-06 16:06:58 -0500

    Add files via upload

--- src/test/scala/com/eva/app/UserJoinTransactionTest.scala
+++ src/test/scala/com/eva/app/UserJoinTransactionTest.scala
@@ -6,16 +6,15 @@ import org.apache.spark.rdd.RDD
 import org.junit.Assert
 import org.scalatest.BeforeAndAfter
 import org.scalatest.FunSuite
-import scala.collection.mutable.WrappedArray
-
 
 import com.eva.app.UserJoinTransactions
+import com.eva.app.UserTranxtnReport
 
+//@Ignore
 class UserJoinTransactionTest extends FunSuite with BeforeAndAfter {
   var sc: SparkContext = _
   val transactionsIn = "C:\\workspace\\transactions.txt"; //args(1)
   val usersIn = "C:\\workspace\\users.txt"; // args(0)
-  val userTransaction = new UserJoinTransactions(sc)
      
         
   before {
@@ -27,16 +26,38 @@ class UserJoinTransactionTest extends FunSuite with BeforeAndAfter {
 
   
   test(" testing if the join was okay ") {
-    val result = userTransaction.run(transactionsIn, usersIn)
-    //wordsRDD.count() should be ("8")
+    val userTransactionReport = new UserJoinTransactions(sc)
+
+    val result = userTransactionReport.run(transactionsIn, usersIn)
+    // equi join so all 3 records
     Assert.assertEquals(result.count(), 2)
-    //assert transaction number 1 has transaction number 3 
-    Assert.assertEquals(result.lookup("1").mkString(","), "3")
-    //assert transaction number 1 has transaction number 3 
-    Assert.assertEquals(result.lookup("2").mkString(","), "1")
+    //assert key number 1 has transaction number 3 
+    //Assert.assertEquals(result.lookup("2").mkString(","), "3")
+    //assert key number 1 has transaction number 3 
+    //Assert.assertEquals(result.lookup("2").mkString(","), "1")
 
   }
 
+  test(" testing if the user x transaction report was crated ") {
+    val userTransaction = new UserTranxtnReport(sc)
+
+    val result = userTransaction.run(transactionsIn, usersIn)
+    // 3 users and 3 transactions
+    Assert.assertEquals(result.keys.count(),3)
+    //assert transaction number 1 has transaction number 3 
+    
+    val arr = result.lookup(1).mkString(",")
+    
+    
+    
+    println("===============> " + arr )
+   // could be better way
+    println(arr.split(",")(2))
+    Assert.assertEquals(arr.split(",")(2).replace("(",""), "matthew@test.com" )
+   
+
+  }
+  
 
   
   after {
