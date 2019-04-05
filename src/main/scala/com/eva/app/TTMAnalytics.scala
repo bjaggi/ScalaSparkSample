0385258a5fc9d3edc6bcb5ed742bc5a1049a3310
Brijesh Jaggi <brijesh.jaggi@gmail.com>
2019-04-05 16:51:59 -0400

    Update TTMAnalytics.scala

--- src/main/scala/com/eva/app/TTMAnalytics.scala
+++ src/main/scala/com/eva/app/TTMAnalytics.scala
@@ -1,46 +1,36 @@
 package com.eva.app
 
-import org.apache.spark._
 import org.apache.spark.rdd.RDD
-import org.apache.spark.SparkContext
-import org.apache.spark.sql.execution.streaming.FileStreamSource.Timestamp
 import org.apache.spark.sql.types.{DataTypes, TimestampType}
+import org.apache.log4j._
+import org.apache.spark.sql.SaveMode
+
 /* SimpleApp.scala */
 import org.apache.spark.sql.SparkSession
 
-class TTMAnalytics {
-  //methods have been moved to a class to unit test it
-  def getWordsByLine(wordsByLine: RDD[String]): RDD[(String)] = {
-    wordsByLine.flatMap(line => line.split(" "))
-
-    /*val counts = wordsByLine.flatMap(line => line.split(" "))
-      .map(word => (word, 1))
-      .reduceByKey(_ + _)*/
-
-  }
-}
-
 object TTMAnalytics {
-
+  //Not required on Mac, Windows Only
   System.setProperty("hadoop.home.dir", "C:\\Softwares\\WinUtils\\");
 
   /**
-   * Main method is for counting word using spark submit
-   */
+    * Main method is for counting word using spark submit
+    */
   def main(args: Array[String]): Unit = {
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
 
     // Use new SparkSession interface in Spark 2.0
-    val spark =  SparkSession
+    val spark = SparkSession
       .builder
       .master("local")
       .appName("TTMAnalytics-APP")
-      .config("spark.some.config.option", "some-value")
+      .config("spark.hadoop.hive.cli.print.header", "true")
+      .config("header", "true")
+      //.config("delimiter","")
       .getOrCreate()
-      //.config("spark.sql.warehouse.dir", "file:///C:/temp") // Necessary to work around a Windows
 
 
-    // Convert our csv file to a DataSet, using our Person case
-    // class to infer the schema.
+    // Convert our csv file to a DataSet, using our ClickStream case class to infer the schema.
     import spark.implicits._
     //val lines = spark.sparkContext.textFile("SampleInputData/2015_07_22_mktplace_shop_web_log_sample.log.gz")
     val lines = spark.sparkContext.textFile("SampleInputData/sample_click_stream.txt")
@@ -54,56 +44,88 @@ object TTMAnalytics {
     val extractedClickStreamGroupedByIP = extractedClickStream.groupBy("clientIpPort")
     println(" Number of Site Visited Per User  ")
     extractedClickStreamGroupedByIP.count().orderBy($"count".desc).show(20)
+
+
     //Goal2 :Determine the average session time
     //https://aws.amazon.com/blogs/big-data/create-real-time-clickstream-sessions-and-run-analytics-with-amazon-kinesis-data-analytics-aws-glue-and-amazon-athena/
     //extractedClickStreamGroupedByIP.avg("timeStamp").show(10)
     import org.apache.spark.sql.functions._
     import org.apache.spark.sql.expressions._
     spark.conf.set("spark.sql.session.timeZone", "UTC")
-
     val extracted2 = extractedClickStream
-      .withColumn("timeStampTS",  unix_timestamp($"timeStamp", "yyyy-MM-dd'T'HH:mm:ss").cast(TimestampType))
-                                                                     //2015-07-22T09:00:28.019143Z
-      .withColumn("timeStamp_ms",  unix_timestamp($"timeStamp", "yyyy-MM-dd'T'HH:mm:ss").cast(DataTypes.LongType))
+      .withColumn("timeStampTS", unix_timestamp($"timeStamp", "yyyy-MM-dd'T'HH:mm:ss").cast(TimestampType))
+      .withColumn("timeStamp_ms", unix_timestamp($"timeStamp", "yyyy-MM-dd'T'HH:mm:ss").cast(DataTypes.LongType))
 
-extracted2.cache()
+    extracted2.cache()
     val extracted3 = extracted2
       .groupBy("clientIpPort")
       .agg(max("timeStamp_ms") - min("timeStamp_ms"))
-      .toDF("clientIpPort", "_2").withColumnRenamed("_2","Session_Time")
+      .toDF("clientIpPort", "_2").withColumnRenamed("_2", "Session_Time")
       .filter($"Session_Time" > 0)
       .orderBy($"Session_Time".desc)
-      //.show(50)
+    //.show(50)
 
     extracted3.show(10);
-extracted3.cache()
+    extracted3.cache()
 
+    println("\n\n\n")
+    println(" Average Session Time ")
     extracted3.agg(
       avg($"Session_Time").as("Avg_Session_Time")
     ).show()
+    extracted3.coalesce(1).write
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .mode(SaveMode.Overwrite)
+      .csv("OutPutProcessedData/goal_2_average_session_time.txt")
 
-    /*extracted2.select($"clientIpPort",$"timestamp")
-      .withColumn("timestamp",max($"timestamp"))
-      .groupBy($"clientIpPort", $"timestamp")
-      //.orderBy($"timestamp".desc).show(10)*/
 
     //Goal3 :Determine unique URL visits per session. To clarify, count a hit to a unique URL only once per session.
+    println("\n\n\n")
+    println(" Determine unique URL visits per session ")
+    val extractedClickStreamGroupedByIPURL = extractedClickStream.groupBy("clientIpPort")
+      //extractedClickStreamGroupedByIPURL.agg(collect_set("url"))
+      .agg(collect_set("url")).show(20, false)
+
+
+
+    // Infer the schema, and register the DataSet as a table.
+    import spark.implicits._
+    extractedClickStream.createOrReplaceTempView("click_stream")
+    val sortedUniqueUrl = spark.sql("SELECT clientIpPort , count(distinct url) as distinct_url_count, collect_set(url) as UNIQUE_URLS   FROM click_stream " +
+      "                                        GROUP BY clientIpPort Order By distinct_url_count desc ")
+
+    // Print on the Console
+    //val results = sortedUniqueUrl.collect()
+    //results.foreach(println)
+
+    //Write to File
+    /*    sortedUniqueUrl.coalesce(1).write
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .mode(SaveMode.Overwrite)
+      .csv("OutPutProcessedData/goal_3_unique_visit_per_session.txt")*/
+
 
     //Goal4 : Find the most engaged users, ie the IPs with the longest session times
+    println("\n\n\n")
+    println(" Most engaged users, ie the IPs with the longest session times")
+    extracted3.show(20)
+    extracted3.coalesce(1).write
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .mode(SaveMode.Overwrite)
+      .csv("OutPutProcessedData/goal_4_most_active_users.txt")
 
-
-
-
+    spark.stop()
 
   }
 
-//timestamp elb client:port backend:port request_processing_time backend_processing_time response_processing_time
-   // timestamp   0
-  // client:port is clients IpAddress & port  2
-  // backend_processing_time is the time taken  5
-  // request : url visited : 11
-//case class ClickStream(timeStamp:String, elb:String, backEndIpPort:String, reqProcessingTime:String, backendProcessingTime : String )
-case class ClickStream(timeStamp:String, clientIpPort : String, backendProcessingTime : String , url : String )
+  /**
+    *
+    * @param line
+    * @return
+    */
   def mapper(line:String): ClickStream = {
     val parts = line.split(" ")
 
@@ -111,9 +133,13 @@ case class ClickStream(timeStamp:String, clientIpPort : String, backendProcessin
     return clickStream
   }
 
+  /**
+    *
+    * @param timeStamp
+    * @param clientIpPort
+    * @param backendProcessingTime
+    * @param url
+    */
+  case class ClickStream(timeStamp:String, clientIpPort : String, backendProcessingTime : String , url : String )
+
 }
-
-
-/**
-timestamp elb client:port backend:port request_processing_time backend_processing_time response_processing_time elb_status_code backend_status_code received_bytes sent_bytes "request" "user_agent" ssl_cipher ssl_protocol
-*/
